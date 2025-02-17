2bf52af0aa1e73e38a00fd3ac58cd44b4e4a33c9
st-pardon <okc4pardon@gmail.com>
2025-02-17 13:47:01 +0100

    :white_check_mark: added more methods for user controller

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -5,10 +5,21 @@ import { User, UserInfo } from '../../models/entity.model';
 import { UserAttributes, UserInfoAttributes } from '../../interface/attributes';
 
 export class UserController {
-    static async update(req: Request & { user?: any }, res: Response) {
+    /**
+     * update user endpoint
+     * @param {Request} req - request object
+     * @param {Response} res - response object
+     * @returns {Promise<void>} - response object
+     */
+    static async update(
+        req: Request & { user?: any },
+        res: Response
+    ): Promise<void> {
         try {
+            const { id } = req.params;
+            const userid = id ? id : req.user.userid; // get user id from params or token
+
             const { phone, address, country, firstname, lastname } = req.body;
-            const { userid } = req.user;
             const userInfo = {} as UserInfoAttributes;
             const user = {} as UserAttributes;
 
@@ -19,17 +30,166 @@ export class UserController {
 
             if (firstname) user['firstname'] = firstname;
             if (lastname) user['lastname'] = lastname;
-            console.log(userInfo, user)
-            const data = await UserInfo.update(userInfo, {where: {userid}});
-            const userData = await User.update(user, { where: { userid } });
 
-            console.log(data, userData)
-            res.status(201).json({...data, ...userData})
+            // const data = await UserInfo.update(userInfo, { where: { userid }, returning: true });
+            // const userData = await User.update(user, { where: { userid }, returning: true });
+            // const updatedUserInfo = data[1][0];
+            // const updatedUser = userData[1][0];
+
+            await UserInfo.update(userInfo, { where: { userid } });
+            await User.update(user, { where: { userid } });
+
+            res.status(201).json({ message: 'User updated successfully' });
         } catch (error: any) {
             res.status(500).send({
                 message:
                     error.message ||
                     'Some error occurred while creating the User.',
+            });
+        }
+    }
+
+    /**
+     * get user endpoint
+     * @param {Request} req - request object
+     * @param {Response} res - response object
+     * @returns {Promise<void>} - response object
+     */
+    static async get(
+        req: Request & { user?: any },
+        res: Response
+    ): Promise<void> {
+        try {
+            const { id } = req.params;
+            const userid = id ? id : req.user.userid; // get user id from params or token
+            let data;
+
+            const userData = await User.findOne({
+                where: { userid },
+                attributes: { exclude: ['password'] },
+                include: [
+                    {
+                        model: UserInfo,
+                        as: 'userinfo',
+                        attributes: {
+                            exclude: ['created_at', 'updated_at', 'userid'],
+                        },
+                    },
+                ],
+            });
+
+            // combine user and userinfo data into one object for response
+            if (userData && userData.userinfo) {
+                const { userinfo, ...userWithoutUserInfo } = userData.toJSON();
+                data = { ...userWithoutUserInfo, ...userinfo };
+            } else {
+                data = userData;
+            }
+
+            if (!userData) {
+                res.status(404).send({
+                    message: 'User Not found.',
+                });
+                return;
+            }
+
+            res.status(200).send(data);
+        } catch (error: any) {
+            res.status(500).send({
+                message:
+                    error.message ||
+                    'Some error occurred while retrieving User.',
+            });
+        }
+    }
+
+    /**
+     * delete user endpoint
+     * @param {Request} req - request object
+     * @param {Response} res - response object
+     * @returns {Promise<void>} - response object
+     */
+    static async delete(
+        req: Request & { user?: any },
+        res: Response
+    ): Promise<void> {
+        try {
+            const { id } = req.params;
+            const userid = id ? id : req.user.userid; // get user id from params or token
+
+            await UserInfo.destroy({ where: { userid } });
+            await User.destroy({ where: { userid } });
+
+            res.status(200).send({
+                message: 'User deleted successfully!',
+            });
+        } catch (error: any) {
+            res.status(500).send({
+                message:
+                    error.message ||
+                    'Some error occurred while deleting the User.',
+            });
+        }
+    }
+
+    /**
+     * get all users endpoint
+     * @param {Request} req - request object
+     * @param {Response} res - response object
+     * @returns {Promise<void>} - response object
+     */
+    static async getAll(
+        req: Request & { user?: any },
+        res: Response
+    ): Promise<void> {
+        try {
+            const userData = await User.findAll({
+                attributes: { exclude: ['password'] },
+                include: [
+                    {
+                        model: UserInfo,
+                        as: 'userinfo',
+                        attributes: {
+                            exclude: ['created_at', 'updated_at', 'userid'],
+                        },
+                    },
+                ],
+            });
+
+            res.status(200).send(userData);
+        } catch (error: any) {
+            res.status(500).send({
+                message:
+                    error.message ||
+                    'Some error occurred while retrieving Users.',
+            });
+        }
+    }
+
+    /**
+     * deactivate user endpoint
+     * @param {Request} req - request object
+     * @param {Response} res - response object
+     * @returns {Promise<void>} - response object
+     */
+    static async deactivate(
+        req: Request & { user?: any },
+        res: Response
+    ): Promise<void> {
+        try {
+            const { id } = req.params;
+            const userid = id ? id : req.user.userid; // get user id from params or token
+
+            await User.update({ isActive: false }, { where: { userid } });
+
+            res.status(200).send({
+                message: 'User deactivated successfully!',
+            });
+        } catch (error: any) {
+            res.status(500).send({
+                message:
+                    error.message ||
+                    'Some error occurred while deactivating the User.',
             });
         }
     }
